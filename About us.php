2df660f09d87d5aa75ddc2a641195f47cf743bc9
DanukaNuwan <danukanuwan.m02@gmail.com>
2023-06-07 18:54:47 +0530

    add

---
+++ About us.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ABOUT US | WILD-LIFE SAFARI</title>
+    <link rel="stylesheet" href="../wild-life/CSS/About us.css">
+   
+    <style>
+
+     body{
+      background-image: url("./IMG/ob4.jpg");
+      background-size: cover;
+    
+       }
+
+    </style>
+</head>
+
+<body>
+<?php require 'header.php'?>
+
+  <div class="topic">
+    <p>ABOUT US</p>
+  </div>
+
+  <div class="content">
+    <p>
+      The Wild-Life Safari website,Srilanka's newest and trusted safari booking site.We believe that Success is measured by the number of satisfied customers and that is always been our endeavor.We choose the best accommodation, the best vehicle to travel and we make the best arrangements for unforgettable tours.We make wonderful arrangements for family tours. We make special arrangements 
+      for group tours.We plan your holiday to fit your budget. We offer a personal touch to your requirements. Our packages are valued for money. Our prices are competitive with respect to hotels & transport. Excellent commendation. Availability of Tour and Travel consultant 24 hrs 7 days a week. Quick Responses.<br/><br/>  We provide , <br/> </p>
+  </div>
+  
+  <div class="list">
+       <p>
+        <ul style="list-style-type:disc;">
+       <li>Bundala National Park Tours</li>
+       <li>Kanneliya Forest Tour</li>
+       <li>Wasgamuwa National Park Tours</li> 
+       <li>Minneriya Natinal Park Tours</li>
+       <li>Sinharaja National Forest Tours</li>
+       <li>Udawalawa National Park Tours</li>
+       <li>Kumana National Park Tours</li>
+       <li>Yala National Park Tours</li>
+      </ul>
+ </p>
+  </div>
+  <?php require 'footer.php'?>
+
+</body>
+</html> 
