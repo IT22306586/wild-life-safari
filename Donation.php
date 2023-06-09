6189ce7134e5c7c542b6d8699afc7ef7ce942eff
DanukaNuwan <danukanuwan.m02@gmail.com>
2023-06-10 03:15:23 +0530

    update

---
+++ Donation.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+    <title> WILD-LIFE SAFARI</title>
+    <link rel="stylesheet" href="Dstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+
+ <body>
+    <h1 class="main"> DONATE</h1>
+    <img src="donation.webp" alt="Lights" class="img-main">
+
+    <p>  <div class="paragraph"><span style="color:rgb(139, 6, 2)">Support us with your donation !</span><br> Without you, Our work would be impossible. Your contribution helps to ensure a better life for animals all around the globe. Every donation counts and together, we can change animals lives. 
+      Thank you!</div></p><br><br><br>
+
+  
+      <div class="Form-header">
+        <h2><u> <span style="color:rgba(9, 29, 119, 0.925)">Donate-Information</span> </u></h2>
+
+  <form method="post" action="/action_page.php">
+    <div class="Div1">
+              <label for="fname" >First name:</label><br>
+                    <input type="text" placeholder="First name" id="fname" name="fname" value=""><br>
+                    <label for="lname">Last name:</label><br>
+                    <input type="text" placeholder="Last name" id="lname" name="lname" value=""><br>
+                    <label for="Mnum">Mobile Number:</label><br>
+                    <input type="tel" placeholder="Mobile number" id="Mnum" name="Mnum" value=""><br>
+                    <label for="emails">E-mail:</label><br>
+                    <input type="email" placeholder="E-mal" id="emails" name="emails" multiple><br>
+                    <label for="amount">Amount:</label><br>
+                    <input type="amount" placeholder="Amount/=" id="amount" name="amount" multiple><br>
+
+
+    </div>               
+      <div class="Div2">
+        <div class="Form-header">
+          <h2><u><span style="color:rgba(9, 29, 119, 0.925)">Payment-Information</span></u></h2>
+          <div class="payment">
+            <label for="Card Number" >Card Number:</label><br>
+                    <input type="text" placeholder="0000 0000 0000 0000" id="Card Number" name="Card Number" value=""><br>
+            <label for="Card Holder" >Card Holder:</label><br>
+                    <input type="text" placeholder="John Snow" id="Card Holder" name="Card Holder" value="">&nbsp&nbsp <br> 
+                    <label for="Expire" >Expire:</label><br>
+                    <input type="text" placeholder="MM/YY" id="Expire" name="Expire" value="">         
+
+    
+
+   
+   <br><br><input type="checkbox" name="set" value="set">Save this Card<br><br>
+   
+   <button type="submit" class="pay now">Pay now</button>
+   <button type="submit" class="cancel">Cancel</button>&nbsp &nbsp &nbsp &nbsp <br><br>
+   <img src="Visa.jpg">
+   <img src="MC.jpg">
+   <img src="CI.jpg">
+   <img src="AE..jpg">
+          
+     </div>
+        </div>
+      </div>
+    </div>
+  </form>
+ </body>
+</html>
