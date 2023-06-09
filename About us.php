1330919d56c2fdb71a219ced43f1ad42f8db6cdb
DanukaNuwan <danukanuwan.m02@gmail.com>
2023-06-09 23:25:53 +0530

    update

--- About us.php
+++ About us.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>ABOUUUT US | WILD-LIFE SAFARI</title>
-    <link rel="stylesheet" href="../wild-life/CSS/About us.css">
-   
+    <title>ABOUT US | WILD-LIFE SAFARI</title>
+    <link rel="stylesheet" href="./CSS/About us.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink:wght@700&family=Libre+Baskerville:wght@700&display=swap">
+
     <style>
 
      body{
@@ -26,11 +28,13 @@
   <div class="content">
     <p>
       The Wild-Life Safari website,Srilanka's newest and trusted safari booking site.We believe that Success is measured by the number of satisfied customers and that is always been our endeavor.We choose the best accommodation, the best vehicle to travel and we make the best arrangements for unforgettable tours.We make wonderful arrangements for family tours. We make special arrangements 
-      for group tours.We plan your holiday to fit your budget. We offer a personal touch to your requirements. Our packages are valued for money. Our prices are competitive with respect to hotels & transport. Excellent commendation. Availability of Tour and Travel consultant 24 hrs 7 days a week. Quick Responses.<br/><br/>  We provide , <br/> </p>
+      for group tours.We plan your holiday to fit your budget. We offer a personal touch to your requirements. Our packages are valued for money. Our prices are competitive with respect to hotels & transport. Excellent commendation. Availability of Tour and Travel consultant 24 hrs 7 days a week. Quick Responses.<br/><br/> </p>
   </div>
   
   <div class="list">
        <p>
+        <h4>WE PROVIDE ,</h4>
+        <br/>
         <ul style="list-style-type:disc;">
        <li>Bundala National Park Tours</li>
        <li>Kanneliya Forest Tour</li>
