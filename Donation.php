53179c62a44a67b7ecd075ceadbabd7e29dfe5a7
Sahan003 <135862104+Sahan003@users.noreply.github.com>
2023-06-11 02:53:12 +0530

    update

--- Donation.php
+++ Donation.php
@@ -26,7 +26,8 @@
      <br/>Support us with your donation !<br> Without you, Our work would be impossible. Your contribution helps to ensure a better life for animals all around the globe. Every donation counts and together, we can change animals lives. 
       Thank you!</p><br><br><br>
 
-     
+
+
     </div>
   
     <div class="Div1">
@@ -34,19 +35,19 @@
    
     <p>Donate-Information</p>
          <label for="fname" >First name:</label><br>
-         <input type="text" placeholder="First name" id="fname" name="firstName" value=""><br>
+         <input type="text" placeholder="First name" id="fname" name="firstName" value="" required><br>
 
          <label for="lname">Last name:</label><br>
-         <input type="text" placeholder="Last name" id="lname" name="lastName" value=""><br>
+         <input type="text" placeholder="Last name" id="lname" name="lastName" value="" required><br>
 
          <label for="Mnum">Mobile Number:</label><br>
-         <input type="tel" placeholder="Mobile number" id="Mnum" name="mobileNumber" value=""><br>
+         <input type="tel" placeholder="Mobile number" id="Mnum" name="mobileNumber" value="" required><br>
 
          <label for="email">E-mail:</label><br>
-         <input type="email" placeholder="E-mail" id="email" name="email" multiple><br>
+         <input type="email" placeholder="E-mail" id="email" name="email" multiple value = "" required><br>
 
          <label for="amount">Amount:</label><br>
-         <input type="amount" placeholder="Amount/=" id="amount" name="amount" multiple><br>
+         <input type="amount" placeholder="Amount/=" id="amount" name="amount" multiple value = "" required><br>
 
          <button type="submit">ENTER USER DETAILS</button>
 
@@ -54,17 +55,20 @@
     </div>
 
     <div class="payment">
-      <form> 
+    <form action="./donorpayment.php" method="post">
           <p>Payment-Information</p>
 
           <label for="Card Number" >Card Number:</label><br>
-          <input type="text" placeholder="0000 0000 0000 0000" id="Card Number" name="Card Number" value=""><br>
+          <input type="text" placeholder="0000 0000 0000 0000" id="Card Number" name="cardNumber" value="" required><br>
 
           <label for="Card Holder" >Card Holder:</label><br>
-          <input type="text" placeholder="John Snow" id="Card Holder" name="Card Holder" value="">&nbsp&nbsp <br> 
+          <input type="text" placeholder="John Snow" id="Card Holder" name="cardHolder" value="" required>&nbsp&nbsp <br> 
           
           <label for="Expire" >Expire:</label><br>
-          <input type="text" placeholder="MM/YY" id="Expire" name="Expire" value="">         
+          <input type="text" placeholder="MM/YY" id="Expire" name="expire" value="" required>  
+          
+          <label for="cvv" >cvv:</label><br>
+          <input type="text" placeholder="cvv" id="cvv" name="cvv" value="" required>
 
     
 
@@ -73,6 +77,9 @@
             
             <button type="submit" class="pay now">Pay now</button>
             <button type="submit" class="cancel">Cancel</button>&nbsp &nbsp &nbsp &nbsp <br><br>
+             
+           
+           
             <img src="./IMG/Visa.jpg">
             <img src="./IMG/MC.jpg">
             <img src="./IMG/CI.jpg">
